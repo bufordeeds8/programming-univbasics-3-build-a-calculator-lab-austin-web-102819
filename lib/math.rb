18a53a4ffa053079bdc1dd4b6c36981c928c4005
Buford Eeds <bufordeeds8@gmail.com>
2019-09-06 18:53:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ p num1 + num2
 end
 
 def subtraction(num1, num2)
-
+p num1 - num2
 end
 
 def division(num1, num2)
-
+p num1 / num2
 end
 
 def multiplication(num1, num2)
-
+p num1 * num2
 end
 
 def modulo(num1, num2)
